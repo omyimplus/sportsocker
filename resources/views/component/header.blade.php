52c4e5e6eace24f0a9290c5985f48a62dc589eea
DESKTOP-SM3G3EC\OmYiM <omyimplusoat@gmail.com>
2020-08-11 16:10:27 +0700

    test bg

---
+++ resources/views/component/header.blade.php
@@ -0,0 +1,90 @@
+<div class="container">
+    <div class="row">
+        <div class="col-4">
+            <img src="images/logo-sportsocker.png" alt="" class="img-fluid logo">
+        </div>
+        <div class="col-8 text-right">
+            <div class="socialBtn">
+                <a href="#">
+                    <img src="images/icon/icon-facebook.png" alt="">
+                </a>
+                <a href="#">
+                    <img src="images/icon/icon-line.png" alt="">
+                </a>
+                <a href="#">
+                    <img src="images/icon/icon-youtube.png" alt="">
+                </a>
+            </div>
+            <div class="mt-3 loginBtn">
+                <a href="#">
+                    <img src="images/icon/btn-google.png" alt="">
+                </a>
+                <a href="#">
+                    <img src="images/icon/btn-facebook.png" alt="">
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <img src="images/line-header.png" alt="" class="img-fluid">
+        </div>
+        <div class="col-12">
+            <nav class="navMain">
+                <ul class="list-inline text-center mb-0">
+                    <li class="list-inline-item">
+                        <a href="#" class="active">หน้าแรก</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#">ดูบอลสด</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#">ข่าวกีฬา</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#">วิเคราะห์บอล</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#">ทีเด็ดบอล</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#">นโยบายเว็บไซต์</a>
+                    </li>
+                </ul>
+            </nav>
+            {{-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand d-md-none" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse text-center" id="navbarSupportedContent">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav> --}}
+        </div>
+        <div class="col-12">
+            <img src="images/line-header.png" alt="" class="img-fluid">
+        </div>
+    </div>
+</div>
